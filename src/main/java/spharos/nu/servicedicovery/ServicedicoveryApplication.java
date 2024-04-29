077bd7b4afd32848db5dd2531edcde1880bf4ea3
I-HYEON <tjwn1408@naver.com>
2024-04-29 11:06:06 +0900

    init: 서비스디스커버리 생성

---
+++ src/main/java/spharos/nu/servicedicovery/ServicedicoveryApplication.java
@@ -0,0 +1,13 @@
+package spharos.nu.servicedicovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicedicoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicedicoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spharos/nu/servicedicovery/ServicedicoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package spharos.nu.servicedicovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicedicoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
