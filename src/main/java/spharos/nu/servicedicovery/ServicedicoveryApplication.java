c9c166478c9d546aa4a58c064287c80b6a344a77
I-HYEON <tjwn1408@naver.com>
2024-04-29 11:09:24 +0900

    chore: 유레카 서버위한 yml 환경 설정

--- src/main/java/spharos/nu/servicedicovery/ServicedicoveryApplication.java
+++ src/main/java/spharos/nu/servicedicovery/ServicedicoveryApplication.java
@@ -2,7 +2,9 @@ package spharos.nu.servicedicovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServicedicoveryApplication {
 
