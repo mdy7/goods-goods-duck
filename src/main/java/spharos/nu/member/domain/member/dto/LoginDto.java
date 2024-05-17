a6346c0d5c57ec378a043acef1f7cc843eda7d3d
wnsvy7203 <wnsvy7203@gmail.com>
2024-05-17 17:57:18 +0900

    Refactor: DTO 스키마 추가

--- src/main/java/spharos/nu/member/domain/member/dto/LoginDto.java
+++ src/main/java/spharos/nu/member/domain/member/dto/LoginDto.java
@@ -1,9 +1,12 @@
 package spharos.nu.member.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class LoginDto {
+	@Schema(description = "유저 아이디")
 	private String userId;
+	@Schema(description = "비밀번호")
 	private String password;
 }
