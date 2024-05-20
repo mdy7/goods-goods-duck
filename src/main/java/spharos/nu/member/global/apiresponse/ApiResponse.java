d60f15d9844801e3b362a9cec64033c08e882d7c
hayoonpark <the.day0619@gmail.com>
2024-05-20 09:50:10 +0900

    Update: apiresponse 수정

--- src/main/java/spharos/nu/member/global/apiresponse/ApiResponse.java
+++ src/main/java/spharos/nu/member/global/apiresponse/ApiResponse.java
@@ -14,8 +14,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ApiResponse<T> {
+
 	private Integer status;
-	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private T result;
 	private String message;
 
