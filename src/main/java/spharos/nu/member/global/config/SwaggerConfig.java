05579659f09290051a36e700b13dfa2bba37801d
wnsvy7203 <wnsvy7203@gmail.com>
2024-05-27 10:47:35 +0900

    Feat: 스웨거 설정

--- src/main/java/spharos/nu/member/global/config/SwaggerConfig.java
+++ src/main/java/spharos/nu/member/global/config/SwaggerConfig.java
@@ -2,25 +2,32 @@ package spharos.nu.member.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String BEARER_TOKEN_PREFIX = "Bearer";
+
     @Bean
     public OpenAPI openAPI() {
+
+        String securityJwtName = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
+        Components components = new Components()
+            .addSecuritySchemes(securityJwtName, new SecurityScheme()
+                .name(securityJwtName)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(BEARER_TOKEN_PREFIX)
+                .bearerFormat(securityJwtName));
+
         return new OpenAPI()
-            .components(new Components())
-            .components(new io.swagger.v3.oas.models.Components())
-            .info(apiInfo());
-    }
-
-
-    private Info apiInfo() {
-        return new Info()
-            .title("Nu - Springdoc 문서")
-            .description("Springdoc을 사용한 Swagger UI")
-            .version("1.0.0");
+            .addSecurityItem(securityRequirement)
+            .components(components)
+            .addServersItem(new Server().url("/"));
     }
 }
