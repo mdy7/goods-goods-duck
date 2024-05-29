bec5dc515f9843e0f536f055b0e99feaa34923a0
Doyeon <ansehdus7@gmail.com>
2024-05-29 17:57:58 +0900

    Chore: cors 설정

---
+++ src/main/java/spharos/nu/gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package spharos.nu.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	public CorsFilter corsWebFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
+	}
+}
