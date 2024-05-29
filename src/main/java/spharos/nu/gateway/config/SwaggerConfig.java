a876c105279ca0b4010950f50cb8eb692220be9c
Doyeon <ansehdus7@gmail.com>
2024-05-29 17:30:22 +0900

    Chore: swagger 관련

---
+++ src/main/java/spharos/nu/gateway/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package spharos.nu.goods.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .components(new Components())
+            .components(new io.swagger.v3.oas.models.Components())
+            .info(apiInfo());
+    }
+
+
+    private Info apiInfo() {
+        return new Info()
+            .title("Nu - Springdoc 문서")
+            .description("Springdoc을 사용한 Swagger UI")
+            .version("1.0.0");
+    }
+}
