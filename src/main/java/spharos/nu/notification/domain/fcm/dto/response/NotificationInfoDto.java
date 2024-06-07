fd9efc186b66fc223efae4d5a32b843f7d4b5a33
Doyeon <ansehdus7@gmail.com>
2024-06-07 15:27:24 +0900

    Feat: 알림 저장, 삭제, 조회, 읽음처리 구현

---
+++ src/main/java/spharos/nu/notification/domain/fcm/dto/response/NotificationInfoDto.java
@@ -0,0 +1,25 @@
+package spharos.nu.notification.domain.fcm.dto.response;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Builder
+public class NotificationInfoDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private boolean isRead;
+    private byte notificationType;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+}
