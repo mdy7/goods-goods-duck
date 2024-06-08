0de203e0273c4d5141059d4162ff4c1bb83fd4e0
SEOIHYEON <tjwn1408@naver.com>
2024-06-08 14:43:04 +0900

    Fix: parma -> pathVariable로 변경

--- src/main/java/spharos/nu/aggregation/domain/wish/controller/WishController.java
+++ src/main/java/spharos/nu/aggregation/domain/wish/controller/WishController.java
@@ -2,6 +2,7 @@ package spharos.nu.aggregation.domain.wish.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +21,7 @@ import spharos.nu.aggregation.global.apiresponse.ApiResponse;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/v1/aggregation")
-@Tag(name = "Wish", description = "aggregation-service에서 마이페이지 관심상품 관련 API document")
+@Tag(name = "Wish", description = "aggregation-service에서 관심(좋아요) 관련 API document")
 public class WishController {
 
 	private final WishService wishService;
@@ -28,7 +29,7 @@ public class WishController {
 	@GetMapping("/{goodsCode}/is-wish")
 	@Operation(summary = "관심(좋아요) 여부 조회", description = "상품코드로 좋아요 여부를 조회합니다")
 	public ResponseEntity<ApiResponse<Boolean>> getIsWish(@RequestHeader(value = "User-Uuid", required = false) String uuid,
-		@RequestParam(value = "goodsCode") String goodsCode) {
+		@PathVariable(value = "goodsCode") String goodsCode) {
 
 		return ApiResponse.success(wishService.getIsWish(uuid, goodsCode), "좋아요 여부 조회 성공");
 	}
