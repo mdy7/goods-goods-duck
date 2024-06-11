5288dc05201d8f608ce9929ac4b92bd45230d23e
hayoonpark <the.day0619@gmail.com>
2024-06-11 10:10:57 +0900

    Feat: 매너덕 조회 path 수정 및 코드 일부 수정

--- src/main/java/spharos/nu/member/domain/member/controller/MyPageNController.java
+++ src/main/java/spharos/nu/member/domain/member/controller/MyPageNController.java
@@ -2,12 +2,15 @@ package spharos.nu.member.domain.member.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import spharos.nu.member.domain.member.dto.response.MannerDuckDto;
 import spharos.nu.member.domain.member.service.MyPageService;
 import spharos.nu.member.global.apiresponse.ApiResponse;
 
@@ -22,5 +25,14 @@ public class MyPageNController {
 	public ResponseEntity<ApiResponse<String>> duplicationCheck(@RequestParam String inputParams) {
 		myPageService.isDuplicatedNick(inputParams);
 		return ApiResponse.success(inputParams, "사용 가능한 닉네임입니다.");
+	}
+
+	// 매너덕 조회
+	@GetMapping("/{userUuid}/manner-duck")
+	@Operation(summary = "회원의 매너덕 조회", description = "마이페이지의 매너덕 상태와 다음 매너덕까지 필요한 점수 데이터")
+	public ResponseEntity<ApiResponse<MannerDuckDto>> getMannerDuck(
+		@PathVariable("userUuid") String userUuid) {
+
+		return ApiResponse.success(myPageService.mannerDuckGet(userUuid), "매너덕 조회 성공");
 	}
 }
