5c7af2cbd599b00d62624a55c8dacb2a226487ed
hayoonPark <the.day0619@gmail.com>
2024-06-12 21:54:44 +0900

    Setting: 회원 점수 카프카 통신 설정

--- src/main/java/spharos/nu/etc/domain/review/kafka/config/ReviewProducerConfig.java
+++ src/main/java/spharos/nu/etc/domain/review/kafka/config/ReviewProducerConfig.java
@@ -1,4 +1,25 @@
 package spharos.nu.etc.domain.review.kafka.config;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import spharos.nu.etc.domain.review.dto.event.MemberScoreEventDto;
+
+@Configuration
 public class ReviewProducerConfig {
+
+	@Value("${spring.kafka.bootstrap-servers}")
+	private String bootstrapServer;
+
+	@Bean
+	public Map<String, Object> reviewProducerConfigs() {
+		return CommonJsonSerializer.getStringObjectMap(bootstrapServer);
+	}
+
 }
