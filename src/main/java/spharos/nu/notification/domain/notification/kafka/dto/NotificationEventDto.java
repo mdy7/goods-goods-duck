7049dcd2a40352bd5a644463ae3857330e1ae3b5
Doyeon <ansehdus7@gmail.com>
2024-06-13 16:58:57 +0900

    Feat: kafka 컨슈머 설정

---
+++ src/main/java/spharos/nu/notification/domain/notification/kafka/dto/NotificationEventDto.java
@@ -0,0 +1,15 @@
+package spharos.nu.notification.domain.notification.kafka.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString
+public class NotificationEventDto {
+    private String title;
+    private String content;
+    private List<String> uuid;
+
+}
