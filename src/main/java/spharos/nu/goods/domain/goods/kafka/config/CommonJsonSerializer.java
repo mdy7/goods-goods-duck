d1fdf8a2c2fad88d55f3391b4079c6ec3c193d6d
SEOIHYEON <tjwn1408@naver.com>
2024-06-16 11:10:08 +0900

    Feat: confluent kafka와 연동

--- src/main/java/spharos/nu/goods/domain/goods/kafka/config/CommonJsonSerializer.java
+++ src/main/java/spharos/nu/goods/domain/goods/kafka/config/CommonJsonSerializer.java
@@ -3,13 +3,17 @@ package spharos.nu.goods.domain.goods.kafka.config;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 public class CommonJsonSerializer {
 
-	static Map<String, Object> getStringObjectMap(String bootstrapServer) {
+	static Map<String, Object> getStringObjectMap(String bootstrapServer, String apiKey, String apiSecret) {
 		Map<String, Object> props = new HashMap<>();
 
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
@@ -18,6 +22,10 @@ public class CommonJsonSerializer {
 		props.put(org.springframework.kafka.support.serializer.JsonSerializer.TYPE_MAPPINGS,
 			"CloseEventDto:spharos.nu.goods.domain.goods.dto.event.CloseEventDto," +
 				"OpenEventDto:spharos.nu.goods.domain.goods.dto.event.OpenEventDto");
+		/*confluent kafka 연결을 위한 설정*/
+		props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG,"SASL_SSL");
+		props.put(SaslConfigs.SASL_MECHANISM,"PLAIN");
+		props.put(SaslConfigs.SASL_JAAS_CONFIG,String.format("org.apache.kafka.common.security.plain.PlainLoginModule required username=\"%s\" password=\"%s\";", apiKey, apiSecret));
 
 		return props;
 	}
--- src/main/java/spharos/nu/goods/domain/goods/kafka/config/GoodsProducerConfig.java
+++ src/main/java/spharos/nu/goods/domain/goods/kafka/config/GoodsProducerConfig.java
@@ -9,18 +9,26 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+import lombok.Getter;
 import spharos.nu.goods.domain.goods.dto.event.CloseEventDto;
 import spharos.nu.goods.domain.goods.dto.event.OpenEventDto;
 
 @Configuration
+@Getter
 public class GoodsProducerConfig {
 
 	@Value("${spring.kafka.bootstrap-servers}")
 	private String bootstrapServer;
 
+	@Value("${spring.kafka.api-key}")
+	private String apiKey;
+
+	@Value("${spring.kafka.api-secret}")
+	private String apiSecret;
+
 	@Bean
 	public Map<String, Object> goodsProducerConfigs() {
-		return CommonJsonSerializer.getStringObjectMap(bootstrapServer);
+		return CommonJsonSerializer.getStringObjectMap(bootstrapServer,apiKey,apiSecret);
 	}
 
 	@Bean
