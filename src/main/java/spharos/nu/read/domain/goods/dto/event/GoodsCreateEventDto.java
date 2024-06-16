d9d370bc28ae6d593915c452534ce6a624a97fb6
SEOIHYEON <tjwn1408@naver.com>
2024-06-17 01:07:09 +0900

    Feat: read관련 consumer 설정 완료

---
+++ src/main/java/spharos/nu/read/domain/goods/dto/event/GoodsCreateEventDto.java
@@ -0,0 +1,24 @@
+package spharos.nu.read.domain.goods.dto.event;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class GoodsCreateEventDto {
+
+	@Schema(description = "상품코드")
+	private String goodsCode;
+	@Schema(description = "판매자uuid")
+	private String sellerUuid;
+	@Schema(description = "낙찰자uuid")
+	private String bidderUuid;
+
+}
