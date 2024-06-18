fc7e3edd0dce4354de26dfe00ac6ca21c2af3370
hayoonpark <the.day0619@gmail.com>
2024-06-18 15:44:58 +0900

    Fix: 등록한 상품 조회 코드 수정

--- src/main/java/spharos/nu/read/domain/goods/service/MyPageService.java
+++ src/main/java/spharos/nu/read/domain/goods/service/MyPageService.java
@@ -1,5 +1,7 @@
 package spharos.nu.read.domain.goods.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import spharos.nu.read.domain.goods.dto.response.GoodsSellResponseDto;
 import spharos.nu.read.domain.goods.dto.response.GoodsSummaryDto;
+import spharos.nu.read.domain.goods.entity.Goods;
 import spharos.nu.read.domain.goods.repository.ReadRepository;
 
 @Service
@@ -19,15 +22,29 @@ public class MyPageService {
 
 	public GoodsSellResponseDto sellGoodsGet(String uuid, Pageable pageable, byte status) {
 
-		Page<GoodsSummaryDto> goodsSummaryPage = readRepository.findGoodsBySellerUuidAndTradingStatusOrderByCreatedAtDesc(
-			uuid, pageable, status);
+		Boolean isDisable = false;
+
+		Page<Goods> goodsSummaryPage = readRepository.findGoodsBySellerUuidAndTradingStatusAndIsDisableOrderByCreatedAtDesc(
+			uuid, pageable, status, isDisable);
+
+		List<GoodsSummaryDto> goodsSummaryList = goodsSummaryPage.getContent().stream()
+			.map(goods -> GoodsSummaryDto.builder()
+				.goodsCode(goods.getGoodsCode())
+				.goodsName(goods.getName())
+				.minPrice(goods.getMinPrice())
+				.openedAt(goods.getOpenedAt())
+				.closedAt(goods.getClosedAt())
+				.tradingStatus(goods.getTradingStatus())
+				.createdAt(goods.getCreatedAt())
+				.build())
+			.toList();
 
 		return GoodsSellResponseDto.builder()
 			.totalCount(goodsSummaryPage.getTotalElements())
 			.nowPage(goodsSummaryPage.getNumber())
 			.maxPage(goodsSummaryPage.getTotalPages())
 			.isLast(goodsSummaryPage.isLast())
-			.goodsList(goodsSummaryPage.getContent())
+			.goodsList(goodsSummaryList)
 			.build();
 	}
 }
