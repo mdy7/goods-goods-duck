1e7f0dcfeadc055013588dc233902dba7fc9035e
SEOIHYEON <tjwn1408@naver.com>
2024-06-19 10:23:55 +0900

    Feat: 메인화면 굿즈리스트 섹션 api 구현

--- src/main/java/spharos/nu/read/domain/goods/repository/ReadRepository.java
+++ src/main/java/spharos/nu/read/domain/goods/repository/ReadRepository.java
@@ -1,5 +1,8 @@
 package spharos.nu.read.domain.goods.repository;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -17,4 +20,10 @@ public interface ReadRepository extends MongoRepository<Goods, String> {
 	Page<Goods> findGoodsBySellerUuidAndTradingStatusAndIsDisableOrderByCreatedAtDesc(String sellerUuid,
 		Pageable pageable, byte status, Boolean isDisable);
 
+	Page<Goods> findByCategoryIdAndTradingStatusInAndIsDisable(Long categoryId, List<Byte> tradingStatus,
+		Boolean isDisable, Pageable pageable);
+
+	List<Goods> findByCategoryIdAndOpenedAtBeforeAndClosedAtAfter(Long categoryId, LocalDateTime now1, LocalDateTime now2, Pageable pageable);
+
+    List<Goods> findByCategoryIdAndOpenedAtBetween(Long categoryId, LocalDateTime startOfTomorrow, LocalDateTime endOfTomorrow, Pageable pageable);
 }
