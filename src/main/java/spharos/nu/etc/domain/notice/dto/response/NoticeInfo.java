430cc6879c3e367d20f5bf3c11d202a27ff7f35a
hayoonpark <the.day0619@gmail.com>
2024-06-24 12:11:07 +0900

    Update: dto 스키마 추가

--- src/main/java/spharos/nu/etc/domain/notice/dto/response/NoticeInfo.java
+++ src/main/java/spharos/nu/etc/domain/notice/dto/response/NoticeInfo.java
@@ -2,6 +2,7 @@ package spharos.nu.etc.domain.notice.dto.response;
 
 import java.time.LocalDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -11,13 +12,18 @@ import lombok.ToString;
 @ToString
 public class NoticeInfo {
 
+	@Schema(description = "공지pk")
 	private Long id;
 
+	@Schema(description = "공지 제목")
 	private String title;
 
+	@Schema(description = "공지 내용")
 	private String content;
 
+	@Schema(description = "생성일자")
 	private LocalDateTime createdAt;
 
+	@Schema(description = "수정일자")
 	private LocalDateTime  updatedAt;
 }
