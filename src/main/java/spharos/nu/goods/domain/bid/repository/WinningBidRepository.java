0df20a173a98cf0b0f6021fecab532db90237d2a
hayoonpark <the.day0619@gmail.com>
2024-06-27 20:46:25 +0900

    Update: 필요없는 import 삭제

--- src/main/java/spharos/nu/goods/domain/bid/repository/WinningBidRepository.java
+++ src/main/java/spharos/nu/goods/domain/bid/repository/WinningBidRepository.java
@@ -2,13 +2,10 @@ package spharos.nu.goods.domain.bid.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import spharos.nu.goods.domain.bid.entity.WinningBid;
-import spharos.nu.goods.domain.goods.dto.response.GoodsCodeDto;
 
 @Repository
 public interface WinningBidRepository extends JpaRepository<WinningBid, Long>, WinningBidRepositoryCustom {
