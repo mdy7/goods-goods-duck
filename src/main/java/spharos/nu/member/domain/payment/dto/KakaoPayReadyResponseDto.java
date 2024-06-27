f102534b43b3e6dbcdeb51083a808eeae527b57b
Doyeon <ansehdus7@gmail.com>
2024-06-27 20:58:59 +0900

    Feat: 모바일 url 추가

--- src/main/java/spharos/nu/member/domain/payment/dto/KakaoPayReadyResponseDto.java
+++ src/main/java/spharos/nu/member/domain/payment/dto/KakaoPayReadyResponseDto.java
@@ -9,6 +9,7 @@ import lombok.ToString;
 @Setter
 @ToString
 public class KakaoPayReadyResponseDto {
-    private String tid;                  //결제 고유 번호
-    private String next_redirect_pc_url; // 리닥이렉트 url
+    private String tid;
+    private String next_redirect_pc_url;
+    private String next_redirect_mobile_url;
 }
